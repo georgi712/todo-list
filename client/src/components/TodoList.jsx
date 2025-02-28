e91a5151155405c42405087db59ab664c653baae
georgi712 <152554904+georgi712@users.noreply.github.com>
2025-02-28 22:53:17 +0200

    Initial setup

---
+++ client/src/components/TodoList.jsx
@@ -0,0 +1,44 @@
+import TodoListItem from "./TodoListItem.jsx";
+
+
+export default function TodoList() {
+    return (
+        <section className="todo-list-container">
+      <h1>Todo List</h1>
+
+      <div className="add-btn-container">
+        <button className="btn">+ Add new Todo</button>
+      </div>
+
+      <div className="table-wrapper">
+
+        {/* <!-- Loading spinner - show the load spinner when fetching the data from the server--> */}
+        {/* <div className="loading-container">
+          <div className="loading-spinner">
+            <span className="loading-spinner-text">Loading</span>
+          </div>
+        </div> */}
+
+        {/* <!-- Todo list table --> */}
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="table-header-task">Task</th>
+              <th className="table-header-status">Status</th>
+              <th className="table-header-action">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {/* <!-- Todo item --> */}
+            <TodoListItem />
+            <TodoListItem />
+            <TodoListItem />
+            <TodoListItem />
+
+          </tbody>
+        </table>
+      </div>
+    </section>
+    )
+}
